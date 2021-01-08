69b7bd4e39386c615ca662397a7f4c3f8e815087
Casey <crouse.casey@gmail.com>
2021-01-08 14:46:32 -0700

    Initial Code Commit

---
+++ src/Persistence/Configuration/PersistenceOptions.cs
@@ -0,0 +1,9 @@
+namespace Persistence.Configuration
+{
+    /// <summary>
+    /// Provides configuration values for the persistence layer.
+    /// </summary>
+    public class PersistenceOptions
+    {
+    }
+}
